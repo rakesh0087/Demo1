18c4ac45390573eab598122987169aa960695e01
ADMIN <ADMIN@DESKTOP-FFQIU3D>
2021-10-06 13:36:37 +0530

    rakesh

--- Git/Git First Project/src/com/cjc/DemoGit.java
+++ Git/Git First Project/src/com/cjc/DemoGit.java
@@ -2,6 +2,12 @@ package com.cjc;
 
 public class DemoGit {
 	
+	public void m1()
+	{
+		System.out.println("This is M1");
+	}
+	
+	
 	public static void main(String[] args)
 	{
 		System.out.println("This is Git First Project");
